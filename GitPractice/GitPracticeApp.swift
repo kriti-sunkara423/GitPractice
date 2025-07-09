fbbf9848801182545abd5eab22f7ec6068f9d88a
Kriti Sunkara <karasunshine0@gmail.com>
2025-07-09 10:38:41 -0400

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Scholar on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
